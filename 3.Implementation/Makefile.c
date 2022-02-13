698bb0e5ff5004dd20a66f74e0a145e9b21a0fad
Kalaithirumaran <98828888+Kalaithirumaran@users.noreply.github.com>
2022-02-13 09:11:48 +0530

    Add files via upload

---
+++ 3.Implementation/Makefile.c
@@ -0,0 +1,9 @@
+#target  : Dependencies
+#<TAB>commands
+
+Build : project_main.c airline reservation.c test_airline reservation. unity.c
+        gcc project_main,c airline resrvation.c test_airline reservation.c unity.c -0 airline reservation.out
+Run : airline reservation.out
+            ./airline reservation.out
+Clean :
+       rm -rf *.0 *.out *.i *.s      
