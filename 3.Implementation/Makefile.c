d2f4df193d0b1797cbad4dc696ada0d68759b741
Kalaithirumaran <98828888+Kalaithirumaran@users.noreply.github.com>
2022-02-21 10:04:37 +0530

    Update Makefile.c

--- 3.Implementation/Makefile.c
+++ 3.Implementation/Makefile.c
@@ -1,9 +1,9 @@
 #target  : Dependencies
 #<TAB>commands
 
-Build : project_main.c airline reservation.c test_airline reservation. unity.c
-        gcc project_main,c airline resrvation.c test_airline reservation.c unity.c -0 airline reservation.out
-Run : airline reservation.out
-            ./airline reservation.out
+Build : project_main.c airline record.c test_airline record. unity.c
+        gcc project_main,c airline record.c test_airline record.c unity.c -0 airline record.out
+Run : airline record.out
+            ./airline record.out
 Clean :
        rm -rf *.0 *.out *.i *.s      
